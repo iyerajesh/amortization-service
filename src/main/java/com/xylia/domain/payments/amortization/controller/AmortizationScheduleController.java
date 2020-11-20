2411fe0f3cc72b738e7f5df02bb2f1e67ef3350a
Rajesh Iyer <iyerajesh@gmail.com>
2020-11-19 20:29:19 -0500

    Added the cross origin header to the rest API

--- src/main/java/com/xylia/domain/payments/amortization/controller/AmortizationScheduleController.java
+++ src/main/java/com/xylia/domain/payments/amortization/controller/AmortizationScheduleController.java
@@ -4,16 +4,14 @@ import com.xylia.domain.payments.amortization.model.AmortizationRequest;
 import com.xylia.domain.payments.amortization.model.AmortizedDataLineItem;
 import com.xylia.domain.payments.amortization.service.AmortizationScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/payments/amortization")
+@CrossOrigin
 public class AmortizationScheduleController {
 
     @Autowired
